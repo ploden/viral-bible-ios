859cdab8419b7ff605fbca2e440e56c0d0b8affc
Alan Young Admin <ryan@Macintosh.local>
2015-10-03 20:20:27 -0700

    table view working

--- Viral-Bible-Ios/ViewController_Custom.swift
+++ Viral-Bible-Ios/ViewController_Custom.swift
@@ -10,13 +10,20 @@ import Foundation
 
 import UIKit
 
-class ViewController_Custom: UIViewController {
+class ViewController_Custom: UIViewController, UITableViewDataSource,UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    var objects=[String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.'
+        
+        self.objects.append("iphone")
+        self.objects.append("iphone2")
+        self.objects.append("iphone3")
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,5 +31,33 @@ class ViewController_Custom: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+        //<#code#>
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return 1
+        
+        return self.objects.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //return UITableViewCell()
+        
+        let aCell=self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! TableViewCell
+        
+        aCell.titleLabel.text=self.objects[indexPath.row]
+        return aCell
+        
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    
+
     
 }
--- Viral-Bible-Ios/Viral-Bible-Ios/TableViewCell.swift
+++ Viral-Bible-Ios/Viral-Bible-Ios/TableViewCell.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
